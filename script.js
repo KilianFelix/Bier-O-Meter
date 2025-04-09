b2ea5df64e1d909aa2fe716292617d8ae17a3261
KilianFelix <68030766+KilianFelix@users.noreply.github.com>
2025-04-09 12:14:08 +0200

    Error Codes

--- script.js
+++ script.js
@@ -3,22 +3,36 @@ function getUrlParameter(name) {
     return urlParams.get(name);
 }
 
-const beerTotal = getUrlParameter('total');
-const beerDrank = getUrlParameter('drank');
-const beerPercent = (beerDrank / beerTotal) * 100;
-const glassHeight = 1080;
-const beerHeight = (glassHeight / 100) * beerPercent;
-
 const beerTexture = document.getElementById("beerTexture");
 const beer = document.getElementById("beer");
 const beerFoam = document.getElementById("beerFoam");
 const beerFoamHeight = 100;
 
-const beerInputDrank = document.getElementById("beerInputDrank");
+function beerChange() {
+    var beerTotal = getUrlParameter('total');
+    var beerDrank = getUrlParameter('drank');
+    var beerPercent = (beerDrank / beerTotal) * 100;
+    var glassHeight = 1080;
+    var beerHeight = (glassHeight / 100) * beerPercent;
 
-document.getElementById("beerTextFull").textContent = ((beerTotal / 100) + " L")
-document.getElementById("beerTextHalf").textContent = ((beerTotal / 100) / 2 + " L")
+    if(beerTotal === null) {
+        console.log("Missing ?total=");
+        document.getElementById("beerTextFull").textContent = "Missing ?total=";
+        return;
+    } 
+    
+    if (beerDrank === null) {
+        console.log("Missing ?drank=");
+        document.getElementById("beerTextHalf").textContent = "Missing ?drank=";
+        return;
+    }
 
-beer.style.height = (beerHeight + "px");
-beer.style.top = (glassHeight - beerHeight - 29 + "px");
-beerFoam.style.top = (glassHeight - beerFoamHeight - beerHeight + "px");
+    document.getElementById("beerTextFull").textContent = ((beerTotal / 100) + " L")
+    document.getElementById("beerTextHalf").textContent = ((beerTotal / 100) / 2 + " L")
+    
+    beer.style.height = (beerHeight + "px");
+    beer.style.top = (glassHeight - beerHeight - 29 + "px");
+    beerFoam.style.top = (glassHeight - beerFoamHeight - beerHeight + "px");
+}
+
+beerChange()
